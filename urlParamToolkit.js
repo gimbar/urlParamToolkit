a19a84a3de7619ad0608bfa45ed576d55dfd5dbd
Gimbar <gimbar@breax.org>
2017-03-06 12:07:21 +0100

    remove side effects

--- urlParamToolkit.js
+++ urlParamToolkit.js
@@ -1,42 +1,39 @@
-function addUrlParam(url, paramName. ParamValue){
-  // this method is failsafe -> it doesn't matter wether this param exists before deleting it
-  // it act's like a filter method (but with side effects)
-  removeUrlParam(ParamName);
-  
-  var newUrlParams = window.location.search;
-  newUrlParams += newUrlParams.length > 0 ? "&" : "?";
+function addUrlParam(urlParams, paramName, ParamValue){
+  // removeUrlParam is failsafe -> it doesn't matter wether this param exists before deleting it
+  // it act's like a filter method
+  var newUrlParams = removeUrlParam(urlParams, ParamName);
 
-  newUrlParams += paramName + encodeURIcomponent(ParamValue);
-  window.location.search = newUrlParams;
+  if(paramName && paramValue){
+    newUrlParams += newUrlParams.length > 0 ? "&" : "?";
+    newUrlParams += paramName + encodeURIcomponent(ParamValue);
   }
+  return newUrlParams;
+}
 
-function removeUrlParam (paramName){
-  var url = window.location.search.substring(1);
-  var urlParams = url.split("&");
+function removeUrlParam (urlParams, paramName){
+  var params = urlParams.substring(1).split("&");
 
   var newUrlParams = "?";
 
-  for (var i = 0; i < urlParams.length; i++){
-    var param = urlParams[i].split("=");
+  for (var i = 0; i < params.length; i++){
+    var param = params[i].split("=");
     if(param[0] != paramName){
       newUrlParams += urlParams[i];
     }
   }
 
   // if there are no url params there's no need for the `?`
-  if (newUrlParams.length >= 1){
+  if (newUrlParams.length <= 1){
     newUrlParams = "";
   }
 
-  window.location.search = newUrlParams;
+  return newUrlParams;
 }
 
-function getUrlParam(paramName){
-  var url = window.location.search.substring(1);
-  var urlVariables = url.split("&");
-
-  for (var i = 0; i < urlVariables.length; i++){
-    var param = urlVariables[i].split("=");
+function getUrlParam(urlParams, paramName){
+  var params = urlParams.substring(1).split("&");
+  for (var i = 0; i < params.length; i++){
+    var param = params[i].split("=");
     if(param[0] === paramName){
       return decodeURIComponent(param[1]);
     }
